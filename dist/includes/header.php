515a78b314527ff632aaaecd742651f1fba66186
Luis Fernando Solano Aguilar <39512309+lsolanoa@users.noreply.github.com>
2021-07-10 11:49:53 -0600

    User View Completed
    index.html -> index.php
    posts.php Template
    pages.php Template
    includes/
    header.php
    footer.php

---
+++ dist/includes/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<!--
+#Project Web Main
+#Creators
+	Luis Fernando Solano Aguilar [luis.solano.aguilar@gmailcom] @lsolanoa
+	Adrián Vergara Heidke [adrianvergaraheidke@ucr.ac.cr]
+#HTML Template
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<!-- Meta -->
+		<title>wp01-ucr-lsolanoa</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="author" content="Luis Fernando Solano Aguilar, Adrián Vergara Heidke">
+		<meta name="keywords" content="mulimodality, academic research, linguistics, ucr, university of costa rica">
+		<!-- Styles -->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Logo -->
+						<h1><a href="index.php" id="logo"> <img class="logo_main left" src="images/logo_main.png" alt="Logo Page Main"></a></h1>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="index.php">Home - [nav_title][nav_type=main]</a></li>
+								<li>
+									<a href="#">Dropdown - [nav_title]</a>
+									<ul>
+										<li><a href="#">Lorem dolor - [nav_title][nav_type=sub]</a></li>
+										<li><a href="#">Magna phasellus - [nav_title][nav_type=sub]</a></li>
+										<li><a href="#">Etiam sed tempus - [nav_title][nav_type=sub]</a></li>
+									</ul>
+								</li>
+                <li><a href="posts.php">Posts - [nav_title][nav_type=main]</a></li>
+                <li><a href="pages.php">Pages - [nav_title][nav_type=main]</a></li>
+							</ul>
+						</nav>
+
+				</div>
